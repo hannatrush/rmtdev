487dbf7155709d6412dce2644222826e857cba71
hannatrush <h4annatrush@gmail.com>
2026-02-04 07:25:19 +0200

    fixed TS error during deployment

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -3,13 +3,18 @@ import { forwardRef } from 'react';
 import JobList from './JobList';
 import { createPortal } from 'react-dom';
 import { useSelectedJobItems } from '../lib/hooks/jobItems';
+import { JobItem } from '../lib/types';
 
 const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useSelectedJobItems();
 
   return createPortal(
     <div ref={ref} className="bookmarks-popover">
-      <JobList jobItems={bookmarkedJobItems || []} isLoading={isLoading} size="small" />
+      <JobList
+        jobItems={(bookmarkedJobItems as JobItem[]) || []}
+        isLoading={isLoading}
+        size="small"
+      />
     </div>,
     document.body,
   );
